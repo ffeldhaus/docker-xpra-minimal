e1675a7d1c7e4995ab8b243764318b0ae38a45f9
Florian Feldhaus <florian.feldhaus@gmail.com>
2020-10-27 21:29:14 +0000

    fixed parsing of docker CMD with arguments in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # start xpra as xpra user with command specified in dockerfile as CMD or passed as parameter to docker run
-runuser -l xpra -c "XPRA_PASSWORD=$XPRA_PASSWORD /usr/bin/xpra start --daemon=no --start-child='$@'"
+CMD="XPRA_PASSWORD=$XPRA_PASSWORD /usr/bin/xpra start --daemon=no --start-child='$@'"
+runuser -l xpra -c "$CMD"
